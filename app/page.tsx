b237013818d5e07f09103627106c4abf702644f2
Zubayer Bin Rais <28019355+ZBRais@users.noreply.github.com>
2024-12-05 23:59:26 +0530

    broken bentoGrid

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { Hero } from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+          {name: 'Home', link: '/', icon: <FaHome />},
+        ]} />
         <Hero />
       </div>
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
